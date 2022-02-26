acb59a71ac4769c7ba305cdd3dad06173864178a
Hubert Jasudowicz <hubert.jasudowicz@gmail.com>
2022-02-26 18:18:01 +0100

    Reformat

--- flake.nix
+++ flake.nix
@@ -6,39 +6,41 @@
   inputs.nixpkgs-xen.url = "github:chivay/nixpkgs/xen-4.16";
 
   outputs = { self, nixpkgs, flake-utils, nixpkgs-xen }:
-  let system = "x86_64-linux";
+    let
+      system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       xenpkgs = nixpkgs-xen.legacyPackages.${system};
-  in
-  {
-    devShell.x86_64-linux = pkgs.mkShell.override { stdenv = pkgs.llvmPackages_13.stdenv; } {
-              shellHook = ''
-              export LIBCLANG_PATH=${pkgs.llvmPackages_13.clang-unwrapped.lib}/lib
-              export AR=llvm-ar
-              '';
-              buildInputs = with pkgs; [
-                xenpkgs.xen_4_16
-                rustc
-                cargo
-                clippy
-                rustfmt
-                llvmPackages_13.llvm.dev
-              ];
-    };
-
-    nixosConfigurations = {
-      test-vm = nixpkgs-xen.lib.nixosSystem {
-        system = system;
-        modules = [
-          (inputs: {
-            documentation.enable = false;
-            documentation.man.enable = false;
-            users.users.root.password = "root";
-            virtualisation.xen.enable = true;
-            system.stateVersion = "22.05";
-          })
+    in
+    {
+      devShell.x86_64-linux = pkgs.mkShell.override { stdenv = pkgs.llvmPackages_13.stdenv; } {
+        shellHook = ''
+          export LIBCLANG_PATH=${pkgs.llvmPackages_13.clang-unwrapped.lib}/lib
+          export AR=llvm-ar
+        '';
+        buildInputs = with pkgs; [
+          xenpkgs.xen_4_16
+          rustc
+          cargo
+          clippy
+          rustfmt
+          llvmPackages_13.llvm.dev
+          nixpkgs-fmt
         ];
       };
+
+      nixosConfigurations = {
+        test-vm = nixpkgs-xen.lib.nixosSystem {
+          system = system;
+          modules = [
+            (inputs: {
+              documentation.enable = false;
+              documentation.man.enable = false;
+              users.users.root.password = "root";
+              virtualisation.xen.enable = true;
+              system.stateVersion = "22.05";
+            })
+          ];
+        };
+      };
     };
-  };
 }
